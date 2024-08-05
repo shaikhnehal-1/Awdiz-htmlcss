9965ccff7958a3e4749678e7270c1cd2e1eef21b
shaikhnehal-1 <shaikhnehal.digital@gmail.com>
2024-08-05 11:51:23 +0530

    javascript Day3

---
+++ Javascript/Day-3/index.js
@@ -0,0 +1,61 @@
+// Driving license Eligibility Checker
+
+// var age = 70;
+
+// if (age <= 0) {
+//     console.log("Invalid Age (Enter Age between 1-60)");
+// } else if (age < 18) {
+//     console.log("Not eligible for Driving licence");
+// } else if (age == 18) {
+//     console.log("Eligible for learning licence");
+// } else if (age > 60) {
+//     console.log("Can't apply for driving licence");
+// } else {
+//     console.log("You are eligible for Driving licence");
+// }
+
+
+// Switch Statements 
+//Syntax :
+// switch (data) {
+//     case 1:
+//         console.log()
+//         break;
+//     case 2:
+//         console.log()
+//         break;
+//     default:
+//         console.log()
+// }
+
+// var pickCar = "Jaguar";
+
+// switch (pickCar) {
+//     case "Jaguar":
+//         console.log("You've Picked Jaguar")
+//         break;
+//     case "BMW":
+//         console.log("You've Picked BMW")
+//         break;
+//     case "Audi":
+//         console.log("You've Picked Audi")
+//         break;
+//     default :
+//         console.log("The car you've picked is not available.")
+// }
+
+
+// Logical Operators
+//&& And operator :
+// if all conditions are true then only it'll return true
+// if any one condition is false it'll return false
+//|| OR operator :
+// if any one condition is true, it'll return true
+//if all conditions are false then only it'll return false
+
+var student1 = 80;
+var student2 = 50;
+var student3 = 20;
+
+console.log(student1 >= 50 && student2 >= 50 && student3 >= 50);
+console.log(student1 >= 50 || student2 >= 50 || student3 >= 50);
